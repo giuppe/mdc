4b570573c0df733b3b18c6a6b99079a57fc3a8c1
giuppe3d <giuppe3d>
2007-10-05 17:29:17 +0000

    added streamconverter

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,12 @@
 #include <iostream>
 #include "common/net_manager.h"
 #include "common/data_chunk.h"
+#include "common/config/commandline_configuration.h"
 #include <cstdlib>
 #include "defs.h"
 #include <SDL/SDL.h>
 #include "common/scheduler.h"
+#include "codecs/codec_registry.h"
 #include "server_action.h"
 #include "client_test_action.h"
 
@@ -20,8 +22,12 @@ using namespace std;
 
 void deinit_all();
 
-int main() {
+void stream_converter(AbstractConfiguration*);
 
+int main(int argc, char** argv)
+{
+
+	
 	atexit(deinit_all);
 
 	SDL_Init(SDL_INIT_TIMER);
@@ -30,7 +36,23 @@ int main() {
 	DEBUG_OUT("!!!Hello World!!!\n");
 	
 	SDL_Delay(1000);
+
+	CodecRegistry* codecReg = CodecRegistry::instance();
 	
+	codecReg->register_codec("text", new TextMDCodec());
+	
+	AbstractConfiguration* config = new CommandlineConfiguration(argc, argv);
+	
+	bool call_convert;
+	
+	config->get_bool("", "convert", call_convert);
+	
+	if(call_convert==true)
+	{
+		stream_converter(config);
+	}
+	else
+	{
 	Scheduler* sched = new Scheduler();
 	ServerAction* server = new ServerAction();
 	
@@ -55,6 +77,9 @@ int main() {
 	delete server;
 	delete client;
 	delete sched;
+	}
+	
+	delete config;
 	
 	return 0;
 }
@@ -66,3 +91,24 @@ void deinit_all()
 	LogManager::instance()->deinit();
 }
 
+
+void stream_converter(AbstractConfiguration* config)
+{
+	std::string output_filename, input_filename;
+	config->get_string("","output", output_filename);
+	config->get_string("","input", input_filename);
+		
+	DEBUG_OUT("converting "<<input_filename<<" to "<<output_filename<<"\n");
+	
+	CodecRegistry* codecReg = CodecRegistry::instance();
+	AbstractMDCodec* codec;
+	codecReg->get_codec("text", codec);
+	TextStream text;
+	text.load_from_disk(input_filename);
+	MDStream mdstream;
+	
+	codec->code(&text, &mdstream);
+	
+	mdstream.save_to_disk(output_filename);
+	
+}
