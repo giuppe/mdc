019890d244e9566a5a66370dc29ee762e13c18ac
giuppe3d <giuppe3d>
2007-12-03 19:04:59 +0000

    *** empty log message ***

---
+++ src/messages/mdc_message_asrq.cpp
@@ -0,0 +1,54 @@
+
+#include "defs.h"
+#include "mdc_message_asrq.h"
+#include <cstring>
+#include <cstdlib>
+
+void MDCMessageAsrq::set_hash(std::string hash)
+{
+	set_parameter_part("h", hash);
+}
+	
+std::string MDCMessageAsrq::get_hash() const
+{
+	return get_parameter_part("h");
+}
+	
+void MDCMessageAsrq::set_flow_id(Uint8 flow_id)
+{
+	char* buffer = new char[255];
+	sprintf(buffer, "%d", flow_id);
+	set_parameter_part("f", std::string(buffer));
+	delete [] buffer;
+}
+	
+Uint8 MDCMessageAsrq::get_flow_id() const
+{
+	return (Uint8) atoi(get_parameter_part("f").c_str());
+}
+	
+void MDCMessageAsrq::set_sequence_begin(Uint32 sequence_begin)
+{
+	char* buffer = new char[255];
+	sprintf(buffer, "%d", sequence_begin);
+	set_parameter_part("sb", std::string(buffer));
+	delete []buffer;
+}
+	
+void MDCMessageAsrq::set_sequence_end(Uint32 sequence_end)
+{
+	char* buffer= new char[255];
+	sprintf(buffer, "%d", sequence_end);
+	set_parameter_part("se", std::string(buffer));
+	delete [] buffer;
+}
+	
+	Uint32 MDCMessageAsrq::get_sequence_begin() const
+	{
+		return (Uint32) atoi(get_parameter_part("sb").c_str());
+	}
+
+	Uint32 MDCMessageAsrq::get_sequence_end() const
+	{
+		return (Uint32) atoi(get_parameter_part("se").c_str());
+	}
--- src/messages/mdc_message_asrq.h
+++ src/messages/mdc_message_asrq.h
@@ -35,6 +35,23 @@ public:
 		m_parameter = "";
 
 	}
+	
+	
+	void set_hash(std::string hash);
+	
+	std::string get_hash() const;
+	
+	void set_flow_id(Uint8 flow_id);
+	
+	Uint8 get_flow_id() const;
+	
+	void set_sequence_begin(Uint32 sequence_begin);
+	
+	void set_sequence_end(Uint32 sequence_end);
+	
+	Uint32 get_sequence_begin() const;
+
+	Uint32 get_sequence_end() const;
 
 	
 };
