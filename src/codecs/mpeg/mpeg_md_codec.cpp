a76771b7a55e248a7a99ebf72f392e9fa3f2f576
livent <livent>
2007-12-11 19:17:16 +0000

    implementazione mpeg-codec che dimostra l'inapplicabilità di tecniche text-codec based

--- src/codecs/mpeg/mpeg_md_codec.cpp
+++ src/codecs/mpeg/mpeg_md_codec.cpp
@@ -15,14 +15,12 @@
  ***************************************************************************/
 
 #include "mpeg_md_codec.h"
-
 #include "defs.h"
 #include "mpeg_stream.h"
 #include "../mdc/md_stream.h"
 #include "../mdc/descriptor.h"
 #include "../../common/data_chunk.h"
 #include "mpeg_codec_parameters.h"
-
 #include <cmath>
 
 MpegMDCodec::MpegMDCodec() {
@@ -37,28 +35,24 @@ void MpegMDCodec::code(AbstractStream* stream, MDStream* md_stream) const {
 	Uint32 descriptors_number = (Uint32)ceil(((double)flow_dimension)/((double)m_preferred_payload_size));
 	Uint16 max_payload_size = (flow_dimension/descriptors_number)+1;
 	md_stream->init(stream->compute_hash_md5(), m_flows_number, descriptors_number);
-	Uint64 offset = 0;
 	for (Uint8 i=0; i<m_flows_number; i++) {
+		Uint64 offset = 0;
 		for (Uint32 j=0; j<descriptors_number; j++) {
-			if (stream_size-offset > 0) {
+			if (stream_size-(offset+i) > 0) {
 				Descriptor* descriptor= new Descriptor();
 				descriptor->set_stream_id(md_stream->get_stream_id());
 				descriptor->set_flow_id(i);
 				descriptor->set_sequence_number(j);
-				descriptor->set_codec_name(string("text"));
+				descriptor->set_codec_name(string("mpeg"));
 				MpegCodecParameters* tcp = new MpegCodecParameters();
 				descriptor->set_codec_parameter(tcp);
 				DataChunk payload;
-				if (offset+max_payload_size < stream_size) {
-					payload += stream->get_data(offset, max_payload_size+1);
-					offset += (max_payload_size+1)*m_flows_number;
-				}
-				else {
-					offset -= max_payload_size+1;
-					payload += stream->get_data(stream_size-offset, stream_size-offset);
-				}
+				if (offset+(m_flows_number*max_payload_size) < stream_size)
+					payload += stream->get_data(offset, max_payload_size);
+				else payload += stream->get_data(offset, stream_size-offset);
 				descriptor->set_payload(payload);
 				md_stream->set_descriptor(descriptor);
+				offset += m_flows_number*max_payload_size;
 			}
 		}
 	}
@@ -81,31 +75,35 @@ void MpegMDCodec::decode(const MDStream* md_stream, AbstractStream* stream) cons
 					if (md_stream->is_valid(descriptor->get_flow_id(), descriptor->get_sequence_number())) {
 						(*dc) += *(descriptor->get_payload());
 						taken_stream.resize(flows_number*sequences_number*(payload_size+1));
-						Uint8* current_received_data;
-						dc->extract_head(payload_size, current_received_data);
-						for (Uint16 k=0; k<payload_size; k++) {
-							taken_stream[k+offset] = current_received_data[k];
-							if (k+offset > max_dimension)
-								max_dimension = k+offset;
+						Uint8* current_received_data = dc->get_data();
+						Uint64 locate_position;
+						for (Uint64 k=0; k<payload_size; k++) {
+							locate_position = offset+k+(payload_size*i);
+							taken_stream[locate_position] = current_received_data[k];
+							if (locate_position > max_dimension)
+								max_dimension = locate_position;
 						}
+						if (i%2 == 0)
+							offset = (i*payload_size)+(flows_number*payload_size*(j+1));
+						else offset = (i*payload_size)+(flows_number*payload_size*j);
 					}
+//					else {
+//						Uint64 k;
+//						for (k=0; k<payload_size; k++) {
+//							Uint64 locate_position = offset+i+flows_number;
+//							taken_stream[locate_position] = 0;
+//						}
+//						offset+=flows_number;
+//					}
 				}
-				else {
-					for (Uint32 k=0; k<payload_size; k++) {
-						taken_stream[k+offset] = 0;
-						if (k+offset > max_dimension)
-							max_dimension = k+offset;
-					}
-				}
-				offset += payload_size*m_flows_number;
 			}
+			DataChunk* taken_dc = new DataChunk();
+			Uint8* temp_container = new Uint8[max_dimension+1];
+			for (Uint64 i=0; i<max_dimension+1; i++)
+				temp_container[i] = taken_stream[i];
+			taken_dc->append(max_dimension+1, temp_container);
+			stream->set_data(*taken_dc);
 		}
-		DataChunk* taken_dc = new DataChunk();
-		Uint8* temp_container = new Uint8[max_dimension+1];
-		for (Uint64 i=0; i<max_dimension+1; i++)
-			temp_container[i] = taken_stream[i];
-		taken_dc->append(max_dimension+1, temp_container);
-		stream->set_data(*taken_dc);
 	}
 }
 
